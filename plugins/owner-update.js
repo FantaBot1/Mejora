9623adda1bc94f554b89ad1b01b191692c3559ee
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-01-27 11:32:00 +0100

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -5,7 +5,7 @@ const handler = async (m, { conn, text }) => {
           const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
           let messager = stdout.toString()
           if (messager.includes('Already up to date.')) messager = '*[❗] No hay nada por actualizar, todo esta actualizado a como esta en el repositorio oficial.*'
-          if (messager.includes('Updating')) messager = '*Alacran Bot A Sido Actualizado :*\n\n' + stdout.toString()
+          if (messager.includes('Updating')) messager = '𝗧𝗨 𝗕𝗢𝗧 𝗦𝗘 𝗛𝗔 𝗔𝗖𝗧𝗨𝗔𝗟𝗜𝗭𝗔𝗗𝗢\n\n' + stdout.toString()
           conn.reply(m.chat, messager, m);
   } catch {      
  try {    
